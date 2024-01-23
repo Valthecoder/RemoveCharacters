60429ca5baaff7056fb4e21b6c89a7091aa22ad0
ValtheCoder <keshia.kdf@gmail.com>
2024-01-23 17:22:16 +0800

    Define remove_chars

---
+++ Removecharacters.py
@@ -0,0 +1,6 @@
+# Define remove_chars
+def remove_characters(word1, n):
+    print("Original word", word1)
+    removed1 = word1[n:]
+    return removed1
+# Print Remove first n characters from a string
