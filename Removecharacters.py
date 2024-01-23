a89c4155c360f4c60226404a8e93766fba0fd9f5
ValtheCoder <keshia.kdf@gmail.com>
2024-01-23 17:25:11 +0800

    Print Remove first n characters from a string

--- Removecharacters.py
+++ Removecharacters.py
@@ -3,4 +3,9 @@ def remove_characters(word1, n):
     print("Original word", word1)
     removed1 = word1[n:]
     return removed1
+
+
 # Print Remove first n characters from a string
+print("Remove first 'n' characters from a string")
+print(remove_characters("pynative", 4))
+print(remove_characters("pynative", 2))
